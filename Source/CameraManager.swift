3596a90d5e8d29d04de4d00013cc8546d1835604
Pablo Garcia <pablo@pablogs.io>
2018-06-04 22:33:51 +0200

    added source code

---
+++ Source/CameraManager.swift
@@ -0,0 +1,217 @@
+//
+//  CameraManager.swift
+//  FrontCameraExample
+//
+//  Created by Pablo on 10/05/2018.
+//  Copyright © 2018 Pablo Garcia. All rights reserved.
+//
+
+import AVFoundation
+import UIKit
+
+protocol CameraManagerDelegate: class {
+
+    func videoHasBeenRecorded(atURL: URL?)
+}
+
+class CameraManager: NSObject {
+
+    enum CameraPreviewError: Swift.Error {
+        case videoInputNotValid
+        case audioInputNotValid
+        case cameraNotFound
+        case microphoneNotFound
+        case noVideoInputAvailable
+        case noAudioInputAvalilable
+        case noVideoOutputAvailable
+    }
+
+    private lazy var captureSession = AVCaptureSession()
+    private var videoPreviewLayer: AVCaptureVideoPreviewLayer?
+    private var inputVideo: AVCaptureInput?
+    private var inputAudio: AVCaptureInput?
+    private var outputMovieFile: AVCaptureMovieFileOutput?
+
+    weak var delegate: CameraManagerDelegate?
+
+    func previewFrontCamera(completion: @escaping (AVCaptureVideoPreviewLayer?, Error?) -> Void) {
+
+        DispatchQueue(label: "configuration").async {
+            do {
+                try self.configureDeviceInputs()
+                try self.configureSessionInputs()
+                try self.configurePhotoOutput()
+                self.addObservers()
+            } catch {
+                DispatchQueue.main.async {
+                    completion(nil, error)
+                }
+                return
+            }
+            DispatchQueue.main.async {
+                self.videoPreviewLayer = AVCaptureVideoPreviewLayer(session: self.captureSession)
+                self.videoPreviewLayer?.videoGravity = AVLayerVideoGravity.resizeAspectFill
+                self.captureSession.startRunning()
+                completion(self.videoPreviewLayer!, nil)
+            }
+        }
+    }
+
+    func stopPreview() {
+
+        captureSession.stopRunning()
+
+    }
+
+    private func configureDeviceInputs() throws {
+
+        let videoDevice = AVCaptureDevice.DiscoverySession(
+            deviceTypes: [AVCaptureDevice.DeviceType.builtInWideAngleCamera],
+            mediaType: AVMediaType.video,
+            position: AVCaptureDevice.Position.front)
+
+        guard let cameraDevice = videoDevice.devices.first else { throw CameraPreviewError.cameraNotFound }
+
+        if cameraDevice.isFocusModeSupported(.autoFocus) {
+            cameraDevice.focusMode = .autoFocus
+        }
+
+        do {
+            inputVideo = try AVCaptureDeviceInput(device: cameraDevice)
+        } catch {
+            throw CameraPreviewError.videoInputNotValid
+        }
+
+        let audioDevices = AVCaptureDevice.DiscoverySession(
+            deviceTypes: [AVCaptureDevice.DeviceType.builtInMicrophone],
+            mediaType: AVMediaType.audio,
+            position: AVCaptureDevice.Position.unspecified)
+
+        guard let audioDevice = audioDevices.devices.first else { throw CameraPreviewError.microphoneNotFound }
+
+        do {
+            inputAudio = try AVCaptureDeviceInput(device: audioDevice)
+        } catch {
+            throw CameraPreviewError.audioInputNotValid
+        }
+    }
+
+    private func configureSessionInputs() throws {
+
+        if self.captureSession.canAddInput(inputVideo!) {
+            self.captureSession.addInput(self.inputVideo!)
+        } else {
+            throw CameraPreviewError.noVideoInputAvailable
+        }
+        if self.captureSession.canAddInput(self.inputAudio!) {
+            self.captureSession.addInput(self.inputAudio!)
+        } else {
+            throw CameraPreviewError.noAudioInputAvalilable
+        }
+    }
+
+    private func configurePhotoOutput() throws {
+
+        self.outputMovieFile = AVCaptureMovieFileOutput()
+
+        if captureSession.canAddOutput(self.outputMovieFile!) {
+            captureSession.addOutput(self.outputMovieFile!)
+        } else {
+            throw CameraPreviewError.noVideoOutputAvailable
+        }
+    }
+
+    public func startRecording() {
+
+        let paths = FileManager.default.urls(for: .documentDirectory, in: .userDomainMask)
+
+        let fileUrl = paths[0].appendingPathComponent("output.mov")
+        try? FileManager.default.removeItem(at: fileUrl)
+
+        outputMovieFile?.startRecording(to: fileUrl, recordingDelegate: self)
+
+    }
+
+    public func stopRecording() {
+
+        outputMovieFile?.stopRecording()
+    }
+
+    public func cameraAuthorization() -> UIAlertController? {
+
+        let cameraMediaType = AVMediaType.video
+        let cameraAuthorizationStatus = AVCaptureDevice.authorizationStatus(for: cameraMediaType)
+        var alertController: UIAlertController?
+
+        switch cameraAuthorizationStatus {
+        case .denied, .restricted:
+            alertController = UIAlertController(title: "Settings",
+                                                message: "Please, press settings to allow us access to your camera",
+                                                preferredStyle: .alert)
+
+            let settingsAction = UIAlertAction(title: "Settings", style: .default) { (_) in
+
+                if let appSettings = NSURL(string: UIApplicationOpenSettingsURLString) {
+                    UIApplication.shared.open(appSettings as URL, options: [:], completionHandler: nil)
+                }
+            }
+            alertController?.addAction(settingsAction)
+
+            let cancelAction = UIAlertAction(title: "Cancel",
+                                             style: .cancel,
+                                             handler: nil)
+            alertController?.addAction(cancelAction)
+
+            return alertController
+        case .authorized: break
+        case .notDetermined:
+            AVCaptureDevice.requestAccess(for: cameraMediaType) { granted in
+                if granted {
+                    print("Granted access to \(cameraMediaType)")
+                } else {
+                    print("Denied access to \(cameraMediaType)")
+                }
+            }
+        }
+        return alertController
+    }
+
+    // MARK: Observers
+
+    private func addObservers() {
+        NotificationCenter.default.addObserver(self,
+                                               selector: #selector(sessionWasInterrupted),
+                                               name: .AVCaptureSessionWasInterrupted,
+                                               object: captureSession)
+        NotificationCenter.default.addObserver(self,
+                                               selector: #selector(sessionInterruptionEnded),
+                                               name: .AVCaptureSessionInterruptionEnded,
+                                               object: captureSession)
+    }
+
+    @objc
+    func sessionWasInterrupted(notification: NSNotification) {
+        print("session was interrupted")
+    }
+
+    @objc
+    func sessionInterruptionEnded(notification: NSNotification) {
+
+        if !self.captureSession.isRunning {
+            self.captureSession.startRunning()
+        }
+    }
+}
+
+extension CameraManager: AVCaptureFileOutputRecordingDelegate {
+
+    func fileOutput(_ output: AVCaptureFileOutput, didFinishRecordingTo outputFileURL: URL,
+                    from connections: [AVCaptureConnection], error: Error?) {
+
+        if error == nil {
+            self.delegate?.videoHasBeenRecorded(atURL: outputFileURL)
+        } else {
+            self.delegate?.videoHasBeenRecorded(atURL: nil)
+        }
+    }
+}
---
+++ Source/FrontCameraView.swift
@@ -0,0 +1,180 @@
+//
+//  FrontCameraView.swift
+//  FrontCameraExample
+//
+//  Created by Pablo on 08/05/2018.
+//  Copyright © 2018 Pablo Garcia. All rights reserved.
+//
+
+import UIKit
+import AVFoundation
+
+protocol FrontCameraDelegate: class {
+    func videoRecorded(atURL: URL?)
+}
+
+class FrontCameraView: UIView {
+
+    enum Position {
+        case upleft
+        case upright
+        case downleft
+        case downright
+    }
+
+    private var videoPreviewLayer: AVCaptureVideoPreviewLayer?
+    private var cameraManager = CameraManager()
+    private var draggGesture: UIPanGestureRecognizer?
+    private var finalPosition = Position.upright
+
+    weak var delegate: FrontCameraDelegate?
+
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+
+        self.draggGesture = UIPanGestureRecognizer(target: self, action: #selector(draggedView(_:)))
+
+        self.addGestureRecognizer(draggGesture!)
+        cameraManager.delegate = self
+
+        translateView(toPosition: .upright)
+    }
+
+    required init?(coder aDecoder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+
+    // MARK: Start and stop camera preview
+    func startPreview(completion: @escaping (Error?) -> Void) {
+
+        cameraManager.previewFrontCamera { (previewLayer, error) in
+            if let previewView = previewLayer {
+                DispatchQueue.main.async {
+                    self.videoPreviewLayer = previewView
+                    self.videoPreviewLayer?.frame = (self.layer.bounds)
+                    self.layer.addSublayer(self.videoPreviewLayer!)
+                    completion(nil)
+                }
+            }
+            if error != nil {
+                DispatchQueue.main.async {
+                    self.backgroundColor = UIColor.green
+                    completion(error)
+                }
+            }
+        }
+    }
+
+    func stopPreview() {
+        cameraManager.stopPreview()
+    }
+
+    // MARK: Check camera authorization
+    public func checkCameraAuthorization() -> UIAlertController? {
+
+        return cameraManager.cameraAuthorization()
+
+    }
+
+    // MARK: moving frontcamera view
+    @objc func draggedView(_ sender: UIPanGestureRecognizer) {
+
+        if !(sender.state == UIGestureRecognizerState.ended) {
+            let translation = sender.translation(in: self.superview)
+            self.center = CGPoint(x: self.center.x + translation.x, y: self.center.y + translation.y)
+            sender.setTranslation(CGPoint.zero, in: self.superview)
+        } else {
+            switch viewPosition() {
+            case .upleft:
+                translateView(toPosition: .upleft)
+            case .upright:
+                translateView(toPosition: .upright)
+            case .downleft:
+                translateView(toPosition: .downleft)
+            case .downright:
+                translateView(toPosition: .downright)
+            }
+        }
+    }
+
+    private func viewPosition() -> Position {
+
+        guard let superView = self.superview else { return Position.upleft}
+
+        let xView = self.center.x
+        let yView = self.center.y
+        let width = superView.frame.size.width/2
+        let height = superView.frame.size.height/2
+
+        if xView<width {
+            if yView < height {
+                return Position.upleft
+            } else {
+                return Position.downleft
+            }
+        } else {
+            if yView < height {
+                return Position.upright
+            } else {
+                return Position.downright
+            }
+        }
+    }
+
+    private func translateView(toPosition: Position) {
+
+        guard let superView = self.superview else { return }
+
+        var origin = CGPoint.zero
+        var finalPlace = CGPoint.zero
+        let offset: CGFloat = 5.0
+        let duration: CFTimeInterval = 0.5
+        let superviewWidth = superView.frame.size.width
+        let superviewHeight = superView.frame.size.height
+
+        switch toPosition {
+        case .upleft:
+            origin = CGPoint(x: 0, y: superView.safeAreaInsets.top)
+            finalPlace = CGPoint(x: offset, y: superView.safeAreaInsets.top+offset)
+        case .upright:
+            origin = CGPoint(x: superviewWidth-self.frame.size.width, y: superView.safeAreaInsets.top)
+            finalPlace = CGPoint(x: superviewWidth-self.frame.size.width-offset,
+                                 y: superView.safeAreaInsets.top+offset)
+        case .downleft:
+            origin = CGPoint(x: 0,
+                             y: superviewHeight-self.frame.size.height-superView.safeAreaInsets.bottom)
+            finalPlace = CGPoint(x: offset,
+                                 y: superviewHeight-self.frame.size.height-offset-superView.safeAreaInsets.bottom)
+        case .downright:
+            origin = CGPoint(x: superviewWidth-self.frame.size.width,
+                             y: superviewHeight-self.frame.size.height-superView.safeAreaInsets.bottom)
+            finalPlace = CGPoint(x: superviewWidth-self.frame.size.width-offset,
+                                 y: superviewHeight-self.frame.size.height-offset-superView.safeAreaInsets.bottom)
+        }
+
+        UIView.animate(withDuration: duration) {
+            self.frame.origin = origin
+        }
+        UIView.animateKeyframes(withDuration: duration/2, delay: duration, options: [], animations: {
+
+            self.frame.origin = CGPoint(x: finalPlace.x, y: finalPlace.y)
+        })
+
+    }
+
+    func startRecording() {
+        self.cameraManager.startRecording()
+    }
+
+    func stopRecording() {
+        self.cameraManager.stopRecording()
+    }
+}
+
+extension FrontCameraView: CameraManagerDelegate {
+
+    func videoHasBeenRecorded(atURL: URL?) {
+        self.delegate?.videoRecorded(atURL: atURL)
+    }
+
+}
